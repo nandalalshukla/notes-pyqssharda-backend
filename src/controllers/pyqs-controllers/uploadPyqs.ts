e36693d815c33fcccfa53bf6ebbaae4520bd050a
Nandalal Shukla <shuklanandalal@gmail.com>
2025-12-19 02:44:22 +0530

    added upload notes code

---
+++ src/controllers/pyqs-controllers/uploadPyqs.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import { Pyq } from "../../models/pyqs/pyq.model";
+
+
+export const uploadPyqs = async (req: Request, res: Response) => {
+  try {
+        const { title, fileUrl, program, courseCode, courseName, semester } =
+        req.body;
+        const userId = req.user!.id;
+        if (
+            !title ||
+            !fileUrl ||
+            !program ||
+            !courseCode ||
+            !courseName ||
+            !semester
+        ) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const newPyq = new Pyq({
+            title,
+            fileUrl,
+            userId,
+            program,
+            courseCode,
+            courseName,
+            semester,
+        });
+        await newPyq.save();
+        res
+            .status(201)
+            .json({ message: "Pyq uploaded successfully", pyq: newPyq });
+    } catch (error) {
+        console.error("Error uploading pyq:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
