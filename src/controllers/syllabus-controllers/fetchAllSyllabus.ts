a9b49c6d28025da25ee14ae5d8a5607053e528f2
Nandalal Shukla <shuklanandalal@gmail.com>
2025-12-20 14:49:33 +0530

    created the fetch all syllbus controller

---
+++ src/controllers/syllabus-controllers/fetchAllSyllabus.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { Pyq } from '../../models/pyqs/pyq.model.js';
+
+export const fetchAllPyqs = async (req: Request, res: Response) => {
+    try {
+        const pyqs = await Pyq.find().sort({ createdAt: -1 });   
+        res.status(200).json({
+            success: true,
+            pyqs
+        });
+    }
+    catch (error) {
+        console.error("Error fetching pyqs:", error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+};
